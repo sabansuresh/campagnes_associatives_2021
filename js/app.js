0672bc846c2e5ded725d47b15cf778d69d672171
harrahx3 <hyacinthe.menard@gmail.com>
2020-12-13 15:03:17 +0100

    eclair

--- js/app.js
+++ js/app.js
@@ -2,9 +2,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // nav menu
     const menus = document.querySelectorAll('.side-menu');
     M.Sidenav.init(menus, {edge: 'left'});
-  });
-  
-function displayinfoListeHeros(){
-  document.getElementById("infoBDEListeHeros").style.display="flex";
-  document.getElementById("containerCards_BDE" ).style.display="none";
-}
+  })
