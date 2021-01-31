73267219dd4d20a6093504fe19235726eb731ce2
harrahx3 <hyacinthe.menard@gmail.com>
2021-01-31 19:39:39 +0100

    commit

--- server.js
+++ server.js
@@ -27,51 +27,51 @@ const gala = require('./objects/GALA/galalPacino.json');
 const relex = require('./objects/Relex/angelaMerkelex.json');
 
 listes = {
-  "BDA": [abordage,{nom: "Gér'art Jugnot",pipo:true,clickable:false,logo:"/BDA/gerart.jpg", standalone: false},{nom: "Artine Marion",pipo:true,clickable:false,logo:"/BDA/artine.jpg", standalone: false},{nom: "Bureau de l'Amour",pipo:true,clickable:false,logo:"/BDA/amour.png", standalone: false}],
-  "BDE": [{ nom: "Les Gosthlisters", logo: "BDE/ghostlisters.png", pipo: false, standalone: true, link:"ghostlisters", clickable : true},
-  { nom: "koh-lanta L'iste des héros", logo: "BDE/listeHeros.jpg", pipo: false, html: "BDE/heros" ,standalone: false, clickable : true  },
-  { nom: "Bureau Des Etoiles", logo: "/BDE/manda.jpg", pipo: true , clickable : false }
+  "BDA": [abordage, { nom: "Gér'art Jugnot", pipo: true, clickable: false, logo: "/BDA/gerart.jpg", standalone: false }, { nom: "Artine Marion", pipo: true, clickable: false, logo: "/BDA/artine.jpg", standalone: false }, { nom: "Bureau de l'Amour", pipo: true, clickable: false, logo: "/BDA/amour.png", standalone: false }],
+  "BDE": [{ nom: "Les Gosthlisters", logo: "BDE/ghostlisters.png", pipo: false, standalone: true, link: "ghostlisters", clickable: true },
+  { nom: "koh-lanta L'iste des héros", logo: "BDE/listeHeros.jpg", pipo: false, html: "BDE/heros", standalone: false, clickable: true },
+  { nom: "Bureau Des Etoiles", logo: "/BDE/manda.jpg", pipo: true, clickable: false }
   ],
-  "BDR":[bdr,{nom: "Bureau du Redoublement",pipo:true,clickable:false,logo:"/BDR/redoublement.png", standalone: false},{nom: "BDéric Zemmour",pipo:true,clickable:false,logo:"/BDR/bdz.jpg", standalone: false}],
-  "ECLAIR": [{ nom: "404 dead Link", logo: "ECLAIR/DeadLink.png", pipo: false , standalone: true, link:"deadlink", clickable : true},
-  { nom: "Adolf Éclair", logo: "ECLAIR/Adolf.png", pipo: true, standalone: false, clickable : true}
+  "BDR": [bdr, { nom: "Bureau du Redoublement", pipo: true, clickable: false, logo: "/BDR/redoublement.png", standalone: false }, { nom: "BDéric Zemmour", pipo: true, clickable: false, logo: "/BDR/bdz.jpg", standalone: false }],
+  "ECLAIR": [{ nom: "404 dead Link", logo: "ECLAIR/DeadLink.png", pipo: false, standalone: true, link: "deadlink", clickable: true },
+  { nom: "Adolf Éclair", logo: "ECLAIR/Adolf.png", pipo: true, standalone: false, clickable: true }
   ],
-  "PAO": [spationautes,{nom: "Pipo Sword Online",pipo:true,clickable:false,logo:"/PAO/pipao.png", standalone: false}],
-  "WEI": [weistern,{nom:"Peaky WEI'nders", logo :"WEI/peaky.png" , pipo:false, standalone:true,link:"peaky", clickable : true}],
-  "PLANET8CO" : [eco,{nom: "Pas-net & Kro",pipo:true,clickable:false,logo:"/ECO/panet.png", standalone: false}],
-  "BI": [birates,teletubies],
+  "PAO": [spationautes, { nom: "Pipo Sword Online", pipo: true, clickable: false, logo: "/PAO/pipao.png", standalone: false }],
+  "WEI": [weistern, { nom: "Peaky WEI'nders", logo: "WEI/peaky.png", pipo: false, standalone: true, link: "peaky", clickable: true }],
+  "PLANET8CO": [eco, { nom: "Pas-net & Kro", pipo: true, clickable: false, logo: "/ECO/panet.png", standalone: false }],
+  "BI": [birates, teletubies],
   "SDeC": [sdec],
-  "RELEX":[relex],
-  "GALA":[gala],
-  "PH":[ph],
-  "CTN":[ctn]
-  }
+  "RELEX": [relex],
+  "GALA": [gala],
+  "PH": [ph],
+  "CTN": [ctn]
+}
 
 const voteDate = '2021-02-04 00:00';
 const endVoteDate = '2021-02-05 00:00';
 
 
 function checkAuthenticated(req, res, next) {
-	
-	if (req.isAuthenticated()) {
-	  return next()
-	}
-	res.redirect('/login')
+
+  if (req.isAuthenticated()) {
+    return next();
   }
+  res.redirect('/login');
+}
 
 async function checkHasVoted(ID) {
-  let sqlCheck = "SELECT * FROM adherents WhERE ID = (?)"
-  res = await sqlite.all(sqlCheck,[ID]);
-  if (res.length==0){
-    console.log("he")
-    return -1
+  let sqlCheck = "SELECT * FROM adherents WHERE ID = (?)"
+  res = await sqlite.all(sqlCheck, [ID]);
+  if (res.length == 0) {
+    console.log("he");
+    return -1;
   }
-  else{
-    if (res[0].voted==1){
-      return 1
+  else {
+    if (res[0].voted == 1) {
+      return 1;
     }
-    if (res[0].voted==0){
-      return 0
+    if (res[0].voted == 0) {
+      return 0;
     }
   }
 }
@@ -81,26 +81,26 @@ const app = express();
 app.set('view-engine', 'ejs');
 app.use(flash());
 app.use(session({
-	secret : process.env.SESSION_SECRET,
-	resave : false,
-	saveUninitialized : false,
-	httpOnly : true,
-	secure:true
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  httpOnly: true,
+  secure: true
 }))
 
 var dir = path.join(__dirname);
 
 
-async function getUserbyEmail(email){
+async function getUserbyEmail(email) {
   let sql = "SELECT * FROM adherents Where Email = (?)";
-	results = await sqlite.all(sql, [email.toLowerCase()]);
-	return results[0];
+  results = await sqlite.all(sql, [email.toLowerCase()]);
+  return results[0];
 }
 
-async function getUserbyId(id){
-	let sql = "SELECT * FROM adherents Where ID = (?)";
-	results = await sqlite.all(sql, [id]);
-	return results[0];
+async function getUserbyId(id) {
+  let sql = "SELECT * FROM adherents Where ID = (?)";
+  results = await sqlite.all(sql, [id]);
+  return results[0];
 }
 
 const initializePassport = require('./passport-config')
@@ -180,99 +180,101 @@ app.get('/peaky', async (req, res) => {
 app.get('/menu', checkAuthenticated, async (req, res) => {
   thisUser = await req.user;
   hasVoted = await checkHasVoted(thisUser.ID);
-  conditionVote = new Date() < new Date(endVoteDate) && (hasVoted==0);
+  console.log(hasVoted);
+  conditionVote = new Date() < new Date(endVoteDate) && (hasVoted == 0);
+  console.log(conditionVote);
   conditionResults = new Date() > new Date(endVoteDate);
-  res.render("loggedIn.ejs",{canVote:conditionVote,canSeeResults : conditionResults}); // générer la page et la renvoyer
+  res.render("loggedIn.ejs", { canVote: conditionVote, canSeeResults: conditionResults }); // générer la page et la renvoyer
 });
 
 app.get('/vote', checkAuthenticated, async (req, res) => {
-  if ((new Date() > new Date(voteDate)) && (new Date() < new Date(endVoteDate))){
+  if ((new Date() > new Date(voteDate)) && (new Date() < new Date(endVoteDate))) {
     thisUser = await req.user;
-    if (await checkHasVoted(thisUser.ID)==0){
-       res.render("vote.ejs", { data: listes }); // générer la page et la renvoyer
+    if (await checkHasVoted(thisUser.ID) == 0) {
+      res.render("vote.ejs", { data: listes }); // générer la page et la renvoyer
     }
-    else{
+    else {
       res.redirect('/')
     }
   }
-  else{
+  else {
     res.redirect("/");
   }
 });
 
 app.get('/results_data', checkAuthenticated, async (req, res) => {
-  if (new Date() > new Date(endVoteDate)){
-  results = await sqlite.all("SELECT * from assos", [])
-  res.json(results); // renvoyer les résultats au format json
+  if (new Date() > new Date(endVoteDate)) {
+    results = await sqlite.all("SELECT * from assos", [])
+    res.json(results); // renvoyer les résultats au format json
   }
-  else{
+  else {
     res.sendStatus(500);
   }
-})
+});
 
 app.get('/udnkjdankjahada', async (req, res) => {
   results = await sqlite.all("SELECT * from assos", [])
   res.json(results); // renvoyer les résultats au format json
-})
+});
 
 app.post('/login', passport.authenticate('local', {
-	successRedirect: '/menu',
-	failureRedirect: '/login',
+  successRedirect: '/menu',
+  failureRedirect: '/login',
   failureFlash: true
-}))
+}));
 
 app.get('/results', async (req, res) => {
-  if (new Date() > new Date(endVoteDate)){
-  res.render("results.ejs", { listes: listes }); // générer la page et la renvoyer
+  if (new Date() > new Date(endVoteDate)) {
+    res.render("results.ejs", { listes: listes }); // générer la page et la renvoyer
   }
   else {
-    res.redirect('/')
+    res.redirect('/');
   }
 });
 
 
 app.post('/vote_post', async (req, res) => {
-  pwd ="aP4B0pcgl94q"
+  pwd = "aP4B0pcgl94q"
   thisUser = await req.user;
   hasVoted = await checkHasVoted(thisUser.ID);
-  if (hasVoted==0){
-  let sql = "SELECT nom AS asso, listes FROM assos";
-  results = await sqlite.all(sql,[]);
-  if (results.length==0){console.log("erreur");res.send(500)}
-  else{
-    for (let cpt=0;cpt<results.length;cpt++){
-      console.log(cpt)
-    row = results[cpt]
-    console.log(row)
-    l = JSON.parse(row.listes);
-    console.log("\nl:\n");
-    console.log(l);
-    console.log(row.asso);
+  if (hasVoted == 0) {
+    let sql = "SELECT nom AS asso, listes FROM assos";
+    results = await sqlite.all(sql, []);
+    if (results.length == 0) { console.log("erreur"); res.send(500); }
+    else {
+      for (let cpt = 0; cpt < results.length; cpt++) {
+        console.log(cpt);
+        row = results[cpt];
+        console.log(row);
+        l = JSON.parse(row.listes);
+        console.log("\nl:\n");
+        console.log(l);
+        console.log(row.asso);
 
-    if (req.body[row.asso] && Object.hasOwnProperty.call(l, req.body[row.asso])) {
-        console.log(req.body[row.asso]);
-        l[req.body[row.asso]].votes += 1;
-        console.log(l[req.body[row.asso]]);
+        if (req.body[row.asso] && Object.hasOwnProperty.call(l, req.body[row.asso])) {
+          console.log(req.body[row.asso]);
+          l[req.body[row.asso]].votes += 1;
+          console.log(l[req.body[row.asso]]);
 
-        let sqlUpdate = "UPDATE assos SET listes=? WHERE nom=? "
-        await sqlite.all(sqlUpdate,[JSON.stringify(l), row.asso]);
+          let sqlUpdate = "UPDATE assos SET listes=? WHERE nom=? "
+          await sqlite.all(sqlUpdate, [JSON.stringify(l), row.asso]);
 
-        var add_has_voted = await sqlite.all("update adherents set voted=true where id=?;",[thisUser.ID]);
+          var add_has_voted = await sqlite.all("update adherents set voted=true where id=?;", [thisUser.ID]);
+        }
       }
     }
+    res.render("index.ejs", { data: listes, showLinkToVotePage: new Date() > new Date(voteDate), showLinkToResultsPage: false }); // générer la page et la renvoyer
   }
-  res.render("index.ejs", { data: listes, showLinkToVotePage: new Date() > new Date(voteDate), showLinkToResultsPage: false }); // générer la page et la renvoyer
-  }
-})
+});
 
 app.get('*', function (req, res) {
   res.redirect('/') // rediriger toutes les pages sur la page principale
 });
 
 app.delete('/logout', async (req, res) => {
-	req.logOut()
-	res.redirect('/')
-})
+  req.logOut()
+  res.redirect('/')
+});
 
 
 var server = http.createServer(app);
